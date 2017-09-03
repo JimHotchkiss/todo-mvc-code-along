908494463b741fc350d83e3dccaa6c945263ea05
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-09-03 15:50:08 +0000

    Added notes.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,9 @@
 class ListsController < ApplicationController
 
   def index
-    @lists = List.all 
+    @lists = List.all
+    # raise @lists.inspect # was the controller able to get the lists from the DB?
+    # This raises an error, if this list object if found
     # implicitly render 'lists/index.html.erb'
   end
 end
